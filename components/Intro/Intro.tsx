26d072b48afe8e8de980b1f3379b0efbf4dab635
Marcin Cholewka <mcholewka@icloud.com>
2023-09-21 19:24:01 +0200

    feat: intro section buttons

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
-import React from "react";
 
 import authorImage from "@/public/marcin_cholewka.jpeg";
 
@@ -49,10 +53,46 @@ export const Intro = () => {
       >
         {t.rich("text", {
           important: (chunks) => <b>{chunks}</b>,
-          italic: (chunks) => <i>{chunks}</i>,
+
           underline: (chunks) => <span className="underline">{chunks}</span>,
         })}
       </motion.p>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          {t("contactMe")}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          href="/CV.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          {t("downloadCV")}
+          <HiDownload className="group-hover:translate-y-1 transition opacity-60" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/marcin-cholewka-15b72615a/"
+          target="__blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/MarcinCholewka"
+          target="__blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.3rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };

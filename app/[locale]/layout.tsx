c64d1874347f75dab733cad091279d919a77d855
Marcin Cholewka <mcholewka@icloud.com>
2023-09-21 21:13:28 +0200

    feat: setup CI, husky and lint staged

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,16 +1,16 @@
-import cx from "classnames";
-import { Inter } from "next/font/google";
-import { notFound } from "next/navigation";
-import { useLocale, useMessages, NextIntlClientProvider } from "next-intl";
+import cx from 'classnames';
+import { Inter } from 'next/font/google';
+import { notFound } from 'next/navigation';
+import { useLocale, useMessages, NextIntlClientProvider } from 'next-intl';
 
-import { Header } from "@/components/Header";
+import { Header } from '@/components/Header';
 
-import "./globals.css";
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 type Params = {
-  locale: "pl" | "en";
+  locale: 'pl' | 'en';
 };
 
 type Props = {
@@ -19,7 +19,7 @@ type Props = {
 };
 
 export const metadata = {
-  title: "Marcin Cholewka",
+  title: 'Marcin Cholewka',
   description: "I'm a frontend developer",
 };
 
@@ -34,12 +34,7 @@ export default function RootLayout({ children, params }: Props) {
 
   return (
     <html lang={locale}>
-      <body
-        className={cx(
-          inter.className,
-          "relative text-gray-950 h-[5000px] pt-24 sm:pt-32"
-        )}
-      >
+      <body className={cx(inter.className, 'relative text-gray-950 h-[5000px] pt-24 sm:pt-32')}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header />
           {children}
